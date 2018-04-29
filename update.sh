693a4716be2f9e46e26d9ae8510252cdc1843df1
Liunian1016 <38773510+Liunian1016@users.noreply.github.com>
2018-04-29 19:17:10 +0800

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+﻿#!/bin/bash
 echo "欢迎使用一键搭建"
 echo "即将搭建的是4.26版本"
 echo "准备开始安装"
 read -p "回车后开始安装"
 echo "请输入你的内网ip" 
 read -p "内网ip： " ip
-cp /root/winnerpubg/restart.sh /root/restart.sh
+cp /root/PUBG-Radar-Onekey/restart.sh /root/restart.sh
 chmod +x restart.sh
 wget --no-check-certificate -O shadowsocks-all.sh https://raw.githubusercontent.com/teddysun/shadowsocks_install/master/shadowsocks-all.sh
 chmod +x shadowsocks-all.sh
@@ -28,8 +28,7 @@ cd libpcap-1.8.1
 make
 make install
 
-git clone https://github.com/794959818/PUBG-Radar-Onekey.git
-cd PUBG-Radar-Onekey/
+git clone https://github.com/Liunian1016/leida.gitcd landonglaji/
 npm i
 npm i -g pino
 npm install -g forever
