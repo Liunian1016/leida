6417f4aeaf816fcb9a4c56e7863f3739a162f738
Liunian1016 <38773510+Liunian1016@users.noreply.github.com>
2018-04-29 19:38:23 +0800

    Add files via upload

--- static/app.js
+++ static/app.js
@@ -682,14 +682,12 @@ const renderMap = () => {
       } else if (playerObj.name) {
         label = playerObj.name
       } else {
-        label = `<${playerObj.name}>`
-      }
-      if (playerObj.kills) {
-        label += ` |杀:${playerObj.kills}|`
+        
+      
+    
       }
     }
-    if (playerObj.health != null) {
-      label += ` |血:${Math.floor(playerObj.health)}|`
+    
     }
     feature.set('_label', label)
     // re-add should be fine
